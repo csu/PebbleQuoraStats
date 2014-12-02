810b2e1051a1130fcede0cb38befe65aef781e48
Christopher Su <christophersu9@gmail.com>
2014-12-01 21:58:56 -0800

    latest

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 var UI = require('ui');
 var ajax = require('ajax');
+var Settings = require('settings');
+
+// Set a configurable with just the close callback
+Settings.config(
+  { url: 'http://christopher.su/PebbleQuoraStats/' },
+  function(e) {
+    console.log('closed configurable');
+
+    // Show the parsed response
+    console.log(JSON.stringify(e.options));
+
+    // Show the raw response if parsing failed
+    if (e.failed) {
+      console.log(e.response);
+    }
+  }
+);
 
 var main = new UI.Card({
     title: 'Quora Stats' 
