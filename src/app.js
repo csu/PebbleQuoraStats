ee9271df85530e5e2d2303ded90447d83a9c3402
Christopher Su <christophersu9@gmail.com>
2014-12-01 22:15:25 -0800

    settings working

--- src/app.js
+++ src/app.js
@@ -2,27 +2,46 @@ var UI = require('ui');
 var ajax = require('ajax');
 var Settings = require('settings');
 
-// Set a configurable with just the close callback
-Settings.config(
-  { url: 'http://christopher.su/PebbleQuoraStats/' },
-  function(e) {
-    console.log('closed configurable');
-
-    // Show the parsed response
-    console.log(JSON.stringify(e.options));
-
-    // Show the raw response if parsing failed
-    if (e.failed) {
-      console.log(e.response);
-    }
-  }
-);
-
 var main = new UI.Card({
     title: 'Quora Stats' 
 });
 
-ajax({ url: 'http://quora-api.herokuapp.com/users/Christopher-J-Su', type: 'json' }, function(data) {
-    main.body('Followers: ' + data.followers + '\nQuestions: ' + data.questions + '\nAnswers: ' + data.answers + '\nEdits: ' + data.edits);
-    main.show();
-});
+var username;
+
+function updateMainCard(card, username) {
+    ajax({ url: 'http://quora-api.herokuapp.com/users/' + username, type: 'json' }, function(data) {
+        card.body('Followers: ' + data.followers + '\nQuestions: ' + data.questions + '\nAnswers: ' + data.answers + '\nEdits: ' + data.edits);
+    });
+}
+
+// Set a configurable with just the close callback
+Settings.config(
+  { url: 'http://christopher.su/PebbleQuoraStats/' },
+  function(e) {
+    // console.log('closed configurable');
+
+    // Show the parsed response
+    // console.log(JSON.stringify(e.options));
+
+    // Show the raw response if parsing failed
+    if (e.failed) {
+        console.log(e.response);
+        main.body('HTTP request failed.');
+    }
+    else {
+        username = Settings.option('username');
+        updateMainCard(main, username);
+    }
+  }
+);
+
+username = Settings.option('username');
+console.log('username: ' + username);
+if ((typeof username === undefined) || (!username)){
+    main.body('Enter your Quora username through the Pebble app.');
+}
+else {
+    updateMainCard(main, username);
+}
+
+main.show();
